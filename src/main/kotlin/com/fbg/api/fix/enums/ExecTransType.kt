6be4416517562e86aa0623a794551b1eff4435d9
chris <chris@newtails.io>
2025-06-24 11:50:57 -0400

    addding order book

--- src/main/kotlin/com/fbg/api/fix/enums/ExecTransType.kt
+++
@@ -1,9 +0,0 @@
-
-package com.fbg.api.fix.enums
-
-enum class ExecTransType(val fixValue: Char) {
-    NEW('0'),
-    CANCEL('1'),
-    CORRECT('2'),
-    STATUS('3')
-}
--- src/main/kotlin/com/fbg/api/fix/enums/LastLiquidityIndicator.kt
+++
@@ -1,9 +0,0 @@
-
-package com.fbg.api.fix.enums
-
-enum class LastLiquidityIndicator(val fixValue: Int) {
-    ADDED_LIQUIDITY(1),
-    REMOVED_LIQUIDITY(2),
-    LIQUIDITY_ROUTED_OUT(3),
-    AUCTION(4)
-}
